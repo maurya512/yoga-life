57d6f3b9a5b10501d01d90c454d4737005629c2d
maurya512 <patelmaurya0512@gmail.com>
2021-10-10 21:57:28 -0400

    data file for Hero component

---
+++ src/data/SliderData.js
@@ -0,0 +1,56 @@
+// ! import and initialize images 
+import Image1 from '../images/yoga1.jpeg'
+import Image2 from '../images/yoga2.jpeg'
+import Image3 from '../images/yoga3.jpeg'
+import Image4 from '../images/yoga4.jpeg'
+import Image5 from '../images/yoga5.jpeg'
+
+
+// ! data for the hero section
+export const SliderData = [
+    {
+        title: 'Lorem Ipsum',
+        description: 'Lorem Ipsum',
+        price: '$499',
+        path: '/yoga',
+        label: 'Lorem Ipsum',
+        image: Image1,
+        alt: 'Lorem Ipsum'
+    },
+    // {
+    //     title: '',
+    //     description: '',
+    //     price: '',
+    //     path: '',
+    //     label: '',
+    //     image: '',
+    //     alt: ''
+    // },
+    // {
+    //     title: '',
+    //     description: '',
+    //     price: '',
+    //     path: '',
+    //     label: '',
+    //     image: '',
+    //     alt: ''
+    // },
+    // {
+    //     title: '',
+    //     description: '',
+    //     price: '',
+    //     path: '',
+    //     label: '',
+    //     image: '',
+    //     alt: ''
+    // },
+    // {
+    //     title: '',
+    //     description: '',
+    //     price: '',
+    //     path: '',
+    //     label: '',
+    //     image: '',
+    //     alt: ''
+    // },
+]
