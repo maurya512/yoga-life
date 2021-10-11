9d77325d4321b7e0b5c765a0ae26390509144b3c
maurya512 <patelmaurya0512@gmail.com>
2021-10-10 21:57:54 -0400

    imported Hero Component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 // ! dependencies
 import React from 'react'
+import Hero from './components/Hero/Hero';
 import Navbar from './components/Navbar/Navbar';
+// ! imports the slider data from data folder
+import { SliderData } from './data/SliderData';
 import GlobalStyle from './globalstyles';
 
 function App() {
   return (
     <>
-    <GlobalStyle />
+      <GlobalStyle />
       <Navbar />
+      {/* pass in props that takes in the data from SliderData */}
+      <Hero slides={SliderData}/>
     </>
   );
 }
